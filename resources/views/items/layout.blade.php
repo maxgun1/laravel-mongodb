9c5da58d739d552f1a331c67feaec3e6dad76ae3
Max Gundarov <max@maximgundarov.com>
2020-09-21 13:27:38 +0300

    fixing header layout navigation bugs

--- resources/views/items/layout.blade.php
+++ resources/views/items/layout.blade.php
@@ -15,7 +15,7 @@
 		<link href="https://unpkg.com/gijgo@1.9.13/css/gijgo.min.css" rel="stylesheet" type="text/css" />
 </head>
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary sticky-top">
-	<a class="navbar-brand" href="{{ url('/items') }}">eBay Scraper 1.0</a>
+	<a class="navbar-brand" href="{{ url('/items') }}">eBay Scraper</a>
 	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
 		aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 		<span class="navbar-toggler-icon"></span>
@@ -24,7 +24,10 @@
 	<div class="collapse navbar-collapse" id="navbarSupportedContent">
 		<ul class="navbar-nav mr-auto">
 			<li class="nav-item active">
-				<a class="nav-link" href="{{ url('/items') }}">Home <span class="sr-only">(current)</span></a>
+				<a class="nav-link" href="{{ url('/') }}">Home <span class="sr-only">(current)</span></a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="{{ url('/weblistings') }}">Finnart Scraper</a>
 			</li>
 			{{-- 		<li class="nav-item">
 		  <a class="nav-link" href="#">Link</a>
